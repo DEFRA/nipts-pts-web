d4626d2fba99c20bf45530b28cb19bc00c881401
masafzal <masood.afzal@capgemini.com>
2024-05-14 11:16:07 +0100

    Minor changes

---
+++ src/Defra.PTS.Web.Domain/Models/PuppeteerSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Defra.PTS.Web.Domain.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class PuppeteerSettings
+    {
+        public string BrowserURL { get; set; }
+
+    }
+}
