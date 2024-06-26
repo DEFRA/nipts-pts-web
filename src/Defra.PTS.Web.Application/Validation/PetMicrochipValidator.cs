b531030eec594e02689d42bd31e10ae76f014694
Rajaram, Shreeni <shreeni.rajaram@capgemini.com>
2024-06-26 16:28:23 +0100

    Microchip Validator

--- src/Defra.PTS.Web.Application/Validation/PetMicrochipValidator.cs
+++ src/Defra.PTS.Web.Application/Validation/PetMicrochipValidator.cs
@@ -1,36 +1,38 @@
 ﻿using FluentValidation;
-using Defra.PTS.Web.Application.Constants;
 using Defra.PTS.Web.Domain.Enums;
 using Defra.PTS.Web.Domain.ViewModels.TravelDocument;
 
-namespace Defra.PTS.Web.Application.Validation
+public class PetMicrochipValidator : AbstractValidator<PetMicrochipViewModel>
 {
-    public class PetMicrochipValidator : AbstractValidator<PetMicrochipViewModel>
+    public PetMicrochipValidator()
     {
-        public PetMicrochipValidator()
+        RuleFor(x => x.Microchipped).NotEmpty().WithMessage("Tell us if your pet is microchipped");
+
+        When(x => x.Microchipped == YesNoOptions.Yes, () =>
         {
-            RuleFor(x => x.Microchipped).NotEmpty().WithMessage("Tell us if your pet is microchipped");
+            RuleFor(x => x.MicrochipNumber).NotEmpty().WithMessage("Enter your pet's microchip number in the correct format");
 
-            When(x => x.Microchipped == YesNoOptions.Yes, () =>
+            When(x => !string.IsNullOrWhiteSpace(x.MicrochipNumber), () =>
             {
-                RuleFor(x => x.MicrochipNumber).NotEmpty().WithMessage("Enter your pet's microchip number");
-
-                When(x => !string.IsNullOrWhiteSpace(x.MicrochipNumber), () =>
-                {
-                    RuleFor(x => x.MicrochipNumber)
-                        .Length(AppConstants.MaxLength.PetMicrochipNumber)
-                        .WithMessage("Enter your pet’s 15-digit microchip number")
-                        .Unless(x => string.IsNullOrEmpty(x.MicrochipNumber) || x.MicrochipNumber.Length == 15);
-
-                    RuleFor(x => x.MicrochipNumber)
-                        .Matches(@"^\d{15}$")
-                        .WithMessage("Enter a 15-digit number, using only numbers")
-                    .When(x => !string.IsNullOrEmpty(x.MicrochipNumber) && x.MicrochipNumber.Length == 15);
-
-
-
-                });
+                RuleFor(x => x.MicrochipNumber)
+                    .Custom((microchipNumber, context) =>
+                    {
+                        if (IsAllDigits(microchipNumber) && microchipNumber.Length != 15)
+                        {
+                            context.AddFailure("MicrochipNumber", "Enter your pet’s 15-digit microchip number");
+                        }
+                        else if (!IsAllDigits(microchipNumber))
+                        {
+                            context.AddFailure("MicrochipNumber", "Enter a 15-digit number, using only numbers");
+                        }
+                    });
             });
-        }
+        });
+    }
+
+
+    private bool IsAllDigits(string microchipNumber)
+    {
+        return microchipNumber.All(char.IsDigit);
     }
 }
