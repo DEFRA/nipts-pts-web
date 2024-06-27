a3a7aca38aff6d92583420dc2c24b4881d9f2e6a
Rajaram, Shreeni <shreeni.rajaram@capgemini.com>
2024-06-27 12:03:06 +0100

    commit

--- src/Defra.PTS.Web.Application/Validation/PetKeeperPostcodeValidator.cs
+++ src/Defra.PTS.Web.Application/Validation/PetKeeperPostcodeValidator.cs
@@ -7,38 +7,40 @@ using MediatR;
 using Microsoft.Extensions.Localization;
 using System.Diagnostics.Metrics;
 
-namespace Defra.PTS.Web.Application.Validation;
-public class PetKeeperPostcodeValidator : AbstractValidator<PetKeeperPostcodeViewModel>
+namespace Defra.PTS.Web.Application.Validation
 {
-    private readonly IMediator _mediator;
-    public PetKeeperPostcodeValidator(IMediator mediator, IStringLocalizer<SharedResource> localizer)
+    public class PetKeeperPostcodeValidator : AbstractValidator<PetKeeperPostcodeViewModel>
     {
-        ArgumentNullException.ThrowIfNull(mediator);
-        _mediator = mediator;
+        private readonly IMediator _mediator;
+        public PetKeeperPostcodeValidator(IMediator mediator, IStringLocalizer<SharedResource> localizer)
+        {
+            ArgumentNullException.ThrowIfNull(mediator);
+            _mediator = mediator;
 
         RuleFor(x => x.Postcode).NotEmpty().WithMessage(x => localizer[$"Enter a postcode"]);
 
-        When(x => !string.IsNullOrWhiteSpace(x.Postcode), () =>
-        {
-            RuleFor(x => x.Postcode)
-                .Custom((postcode, context) =>
-                {
+            When(x => !string.IsNullOrWhiteSpace(x.Postcode), () =>
+            {
+                RuleFor(x => x.Postcode)
+                    .Custom((postcode, context) =>
+                    {
                     if (!System.Text.RegularExpressions.Regex.IsMatch(postcode, AppConstants.RegularExpressions.UKPostcode) ||
                         postcode.Length > AppConstants.MaxLength.Postcode)
-                    {
+                        {
                         context.AddFailure(localizer["Enter a full postcode in the correct format, for example TF7 5AY or TF75AY"]);
-                    }
-                    else if (!BeValidUKPostcode(postcode))
-                    {
+                        }
+                        else if (!BeValidUKPostcode(postcode))
+                        {
                         context.AddFailure(localizer["Enter a postcode in England, Scotland or Wales"]);
-                    }
-                });
-        });
-    }
+                        }
+                    });
+            });
+        }
 
-    private bool BeValidUKPostcode(string postcode)
-    {
-        var result = _mediator.Send(new ValidateGreatBritianAddressRequest(postcode)).Result;
-        return result;
+        private bool BeValidUKPostcode(string postcode)
+        {
+            var result = _mediator.Send(new ValidateGreatBritianAddressRequest(postcode)).Result;
+            return result;
+        }
     }
 }
