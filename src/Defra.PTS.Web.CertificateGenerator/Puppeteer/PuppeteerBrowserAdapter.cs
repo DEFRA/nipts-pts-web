29806fb4bb30e48528dbb3bd3816b3fe02655553
skcapgemini <121160206+skcapgemini@users.noreply.github.com>
2024-07-08 12:38:07 +0100

    Testing pdf with ip

--- src/Defra.PTS.Web.CertificateGenerator/Puppeteer/PuppeteerBrowserAdapter.cs
+++ src/Defra.PTS.Web.CertificateGenerator/Puppeteer/PuppeteerBrowserAdapter.cs
@@ -1,8 +1,10 @@
 ﻿using Defra.PTS.Web.CertificateGenerator.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using PuppeteerSharp;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Defra.PTS.Web.CertificateGenerator.Puppeteer;
@@ -13,18 +15,27 @@ public class PuppeteerBrowserAdapter : IBrowser
 {
     private readonly Launcher launcher;
     private readonly IOptions<ConnectOptions> options;
+    private readonly IConfiguration configuration;
 
-    public PuppeteerBrowserAdapter(Launcher launcher, IOptions<ConnectOptions> options)
+
+    public PuppeteerBrowserAdapter(Launcher launcher, IOptions<ConnectOptions> options, IConfiguration configuration)
     {
         ArgumentNullException.ThrowIfNull(launcher);
         ArgumentNullException.ThrowIfNull(options);
         this.launcher = launcher;
         this.options = options;
+        this.configuration = configuration;
     }
 
     public async Task<IPage> NewPageAsync()
     {
-        var browser = await launcher.ConnectAsync(options.Value);
+        var opt = options.Value;
+        var ipAddressUrl = configuration["IpAddressUrl"];
+        ArgumentNullException.ThrowIfNull(ipAddressUrl);
+        var containerIP = await GetRequestBody(ipAddressUrl);
+        var containerURL = string.Format("http://{0}:3000", containerIP);
+        opt.BrowserURL = containerURL;
+        var browser = await launcher.ConnectAsync(opt);
         try
         {
             var page = await browser.NewPageAsync();
@@ -35,5 +46,13 @@ public class PuppeteerBrowserAdapter : IBrowser
             await browser.DisposeAsync();
             throw;
         }
+    }
+
+    public async Task<string> GetRequestBody(string url)
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        var bodyText = await response.Content.ReadAsStringAsync();
+        return bodyText;
     }
 }
