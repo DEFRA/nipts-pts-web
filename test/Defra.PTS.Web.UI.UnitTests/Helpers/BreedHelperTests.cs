a01e699aff8de5f330332cf7005d83a922f2ee83
Grace Redmayne <grace.redmayne@live.com>
2024-08-22 15:23:27 +0100

    Unit tests

--- test/Defra.PTS.Web.UI.UnitTests/Helpers/BreedHelperTests.cs
+++ test/Defra.PTS.Web.UI.UnitTests/Helpers/BreedHelperTests.cs
@@ -1,25 +1,14 @@
-﻿using Defra.PTS.Web.Application.DTOs.Features;
-using Defra.PTS.Web.Application.DTOs.Services;
+﻿using Defra.PTS.Web.Application.DTOs.Services;
 using Defra.PTS.Web.Application.Features.Lookups.Queries;
 using Defra.PTS.Web.Domain.Enums;
-using Defra.PTS.Web.UI.Controllers;
 using Defra.PTS.Web.UI.Helpers;
-using FluentAssertions.Execution;
+using FluentAssertions;
 using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
-using Microsoft.WindowsAzure.Storage.Blob.Protocol;
 using Moq;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Defra.PTS.Web.UI.UnitTests.Helpers
@@ -39,30 +28,79 @@ namespace Defra.PTS.Web.UI.UnitTests.Helpers
 
         }
 
-        [Test]
-        public async Task GetBreedListTests()
+        [Fact]
+        public async Task GetDogBreedListTests()
         {
             // Arrange
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("cy");
-
             var species = PetSpecies.Dog;
             _mockMediator.Setup(x => x.Send(It.IsAny<GetBreedsQueryRequest>(), CancellationToken.None))
                 .ReturnsAsync(new Application.DTOs.Features.GetBreedsQueryResponse
                 {
                     Breeds = new List<BreedDto>()
                     {
-                        new BreedDto()
-                        {
-                            BreedId = 1,  BreedName = "Afghan Hound"
-                        }
-
-                    }           
+                    new BreedDto() { BreedId = 1, BreedName = "Chihuahua" },
+                    new BreedDto() { BreedId = 2, BreedName = "Pomeranian" },
+                    new BreedDto() { BreedId = 3, BreedName = "Toy Poodle" },
+                    new BreedDto() { BreedId = 4, BreedName = "Italian Greyhound" },
+                    new BreedDto() { BreedId = 5, BreedName = "Whippet" },
+                    }
                 });
+
+            var breed = new List<BreedDto>()
+            {
+                new BreedDto() { BreedId = 1, BreedName = "Chihuahua" },
+                new BreedDto() { BreedId = 2, BreedName = "Pomeranian" },
+                new BreedDto() { BreedId = 3, BreedName = "Toy Poodle" },
+                new BreedDto() { BreedId = 4, BreedName = "Italian Greyhound" },
+                new BreedDto() { BreedId = 5, BreedName = "Whippet" },
+
+            };
 
             // Act
             var result = await _breedHelper.GetBreedList(species);
 
             // Assert
+
+            result.Should().Equal(breed, (b1, b2) => b1.BreedId == b2.BreedId);
+            result.Should().Equal(breed, (b1, b2) => b1.BreedName == b2.BreedName);
+
+        }
+
+        [Fact]
+        public async Task GetCatBreedListTests()
+        {
+            // Arrange
+            var species = PetSpecies.Cat;
+            _mockMediator.Setup(x => x.Send(It.IsAny<GetBreedsQueryRequest>(), CancellationToken.None))
+                .ReturnsAsync(new Application.DTOs.Features.GetBreedsQueryResponse
+                {
+                    Breeds = new List<BreedDto>()
+                    {
+                    new BreedDto() { BreedId = 1, BreedName = "Domestic Shorthair" },
+                    new BreedDto() { BreedId = 2, BreedName = "Siamese" },
+                    new BreedDto() { BreedId = 3, BreedName = "Bengal" },
+                    new BreedDto() { BreedId = 4, BreedName = "Sphynx" },
+                    new BreedDto() { BreedId = 5, BreedName = "Cornish Rex" },
+                    }
+                });
+
+            var breed = new List<BreedDto>()
+            {
+                new BreedDto() { BreedId = 1, BreedName = "Domestic Shorthair" },
+                new BreedDto() { BreedId = 2, BreedName = "Siamese" },
+                new BreedDto() { BreedId = 3, BreedName = "Bengal" },
+                new BreedDto() { BreedId = 4, BreedName = "Sphynx" },
+                new BreedDto() { BreedId = 5, BreedName = "Cornish Rex" },
+
+            };
+
+            // Act
+            var result = await _breedHelper.GetBreedList(species);
+
+            // Assert
+
+            result.Should().Equal(breed, (b1, b2) => b1.BreedId == b2.BreedId);
+            result.Should().Equal(breed, (b1, b2) => b1.BreedName == b2.BreedName);
 
         }
 
