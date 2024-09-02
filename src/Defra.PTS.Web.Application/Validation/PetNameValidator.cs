723a7071bdba5a5959f347ce722b3b238e4a6eaa
Grace Redmayne <grace.redmayne@live.com>
2024-09-02 14:20:33 +0100

    Welsh translation - Pet Name

--- src/Defra.PTS.Web.Application/Validation/PetNameValidator.cs
+++ src/Defra.PTS.Web.Application/Validation/PetNameValidator.cs
@@ -1,17 +1,19 @@
 ﻿using Defra.PTS.Web.Application.Constants;
+using Defra.PTS.Web.Domain;
 using Defra.PTS.Web.Domain.ViewModels.TravelDocument;
 using FluentValidation;
+using Microsoft.Extensions.Localization;
 
 namespace Defra.PTS.Web.Application.Validation;
 public class PetNameValidator : AbstractValidator<PetNameViewModel>
 {
-    public PetNameValidator()
+    public PetNameValidator(IStringLocalizer<SharedResource> localizer)
     {
-        RuleFor(x => x.PetName).NotEmpty().WithMessage("Enter your pet's name");
+        RuleFor(x => x.PetName).NotEmpty().WithMessage(localizer["Enter your pet's name"]);
 
         When(x => !string.IsNullOrWhiteSpace(x.PetName), () =>
         {
-            RuleFor(x => x.PetName).MaximumLength(AppConstants.MaxLength.PetName).WithMessage($"Enter your pet's name, using {AppConstants.MaxLength.PetName} characters or less");            
+            RuleFor(x => x.PetName).MaximumLength(AppConstants.MaxLength.PetName).WithMessage(localizer[$"Enter your pet's name, using {AppConstants.MaxLength.PetName} characters or less"]);            
         });
 
     }
