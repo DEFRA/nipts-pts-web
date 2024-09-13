5e11453c16fb58a06d309d92ffa971923c96be2f
Muhammad Ali Shah <muhammad-ali.shah@capgemini.com>
2024-09-13 07:13:52 +0100

    Welsh for Pet age

--- test/Defra.PTS.Web.Application.UnitTests/Validation/PetAgeValidatorShould.cs
+++ test/Defra.PTS.Web.Application.UnitTests/Validation/PetAgeValidatorShould.cs
@@ -1,8 +1,12 @@
 ﻿using Defra.PTS.Web.Application.Constants;
 using Defra.PTS.Web.Application.Validation;
+using Defra.PTS.Web.Domain;
 using Defra.PTS.Web.Domain.ViewModels.TravelDocument;
 using FluentValidation.TestHelper;
 using MediatR;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -10,62 +14,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Defra.PTS.Web.Application.UnitTests.Validation
+namespace Defra.PTS.Web.Application.UnitTests.Validation;
+
+public class PetAgeValidatorShould
 {
-    public class PetAgeValidatorShould
+    private readonly IStringLocalizer<SharedResource> _localizer;
+
+    public PetAgeValidatorShould()
     {
-        [Fact]
-        public async Task HaveErrorWhenBirthDateEmpty()
-        {
-            var model = new PetAgeViewModel();
-            var validator = new PetAgeValidator();
-            
-            var result = await validator.TestValidateAsync(model);
-            
-            result.ShouldHaveValidationErrorFor(x => x.BirthDate);
-        }
-
-        [Fact]
-        public async Task NotHaveErrorWhenBirthDateHasValue()
-        {
-            var model = new PetAgeViewModel()
-            {
-                Day = 1,
-                Month = 1,
-                Year = 2005,
-                MicrochippedDate = DateTime.Now,
-            };
-            var validator = new PetAgeValidator();
-
-            var result = await validator.TestValidateAsync(model);
-
-            result.ShouldNotHaveValidationErrorFor(x => x.BirthDate);
-            result.ShouldNotHaveValidationErrorFor(x => x.Day);
-            result.ShouldNotHaveValidationErrorFor(x => x.Month);
-            result.ShouldNotHaveValidationErrorFor(x => x.Year);
-        }
-
-        [Fact]
-        public async Task HaveErrorWhenBirthDateExceedsDateLimits()
-        {
-            var model = new PetAgeViewModel()
-            {
-                Day = 1,
-                Month = 1,
-                Year = DateTime.Now.AddYears(-50).Year,
-                MicrochippedDate = DateTime.Now,
-            };
-
-            var validator = new PetAgeValidator();
-
-            var result = await validator.TestValidateAsync(model);
-
-            result.ShouldHaveValidationErrorFor(x => x.BirthDate);
-        }
-
-
-
+        var options = Options.Create(new LocalizationOptions { ResourcesPath = "Resources" });
+        var factory = new ResourceManagerStringLocalizerFactory(options, NullLoggerFactory.Instance);
+        _localizer = new StringLocalizer<SharedResource>(factory);
     }
+
+    [Fact]
+    public async Task HaveErrorWhenBirthDateEmpty()
+    {
+        var model = new PetAgeViewModel();
+        var validator = new PetAgeValidator(_localizer);
+        
+        var result = await validator.TestValidateAsync(model);
+        
+        result.ShouldHaveValidationErrorFor(x => x.BirthDate);
+    }
+
+    [Fact]
+    public async Task NotHaveErrorWhenBirthDateHasValue()
+    {
+        var model = new PetAgeViewModel()
+        {
+            Day = 1,
+            Month = 1,
+            Year = 2005,
+            MicrochippedDate = DateTime.Now,
+        };
+        var validator = new PetAgeValidator(_localizer);
+
+        var result = await validator.TestValidateAsync(model);
+
+        result.ShouldNotHaveValidationErrorFor(x => x.BirthDate);
+        result.ShouldNotHaveValidationErrorFor(x => x.Day);
+        result.ShouldNotHaveValidationErrorFor(x => x.Month);
+        result.ShouldNotHaveValidationErrorFor(x => x.Year);
+    }
+
+    [Fact]
+    public async Task HaveErrorWhenBirthDateExceedsDateLimits()
+    {
+        var model = new PetAgeViewModel()
+        {
+            Day = 1,
+            Month = 1,
+            Year = DateTime.Now.AddYears(-50).Year,
+            MicrochippedDate = DateTime.Now,
+        };
+
+        var validator = new PetAgeValidator(_localizer);
+
+        var result = await validator.TestValidateAsync(model);
+
+        result.ShouldHaveValidationErrorFor(x => x.BirthDate);
+    }
+
 
 
 }
