02c0935116df3a9397c2d33552cefd026a8f247e
Muhammad Ali Shah <muhammad-ali.shah@capgemini.com>
2024-11-14 15:27:58 +0000

    Added cookie consent code

---
+++ src/Defra.PTS.Web.Domain/Models/CookiesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Defra.PTS.Web.Domain.Models;
+
+public class CookiesModel
+{
+    public string GaCookieAcceptYesNo { get; set; } = string.Empty;
+    public string MeasurementId { get; set; } = string.Empty;
+    public bool SeenCookieMessage { get; set; } = false;
+    public bool Saved { get; set; } = false;
+}
